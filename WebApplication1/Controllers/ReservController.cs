2713c34a59a53b7898a4cf6ea599a4b738500bf0
ruey <reneesister@gmail.com>
2024-12-29 15:32:27 +0800

    commit

--- WebApplication1/Controllers/ReservController.cs
+++ WebApplication1/Controllers/ReservController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
-using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -27,48 +25,20 @@ namespace WebApplication1.Controllers
             return View(searchH);
         }
 
-        public async Task<IActionResult> searchDepartment(string hospital)
+        public IActionResult searchDepartment()
         {
-            if (HttpContext.Session.GetString("Account_name") == null)
+            /*if (HttpContext.Session.GetString("Account_name") == null)
             {
                 TempData["message"] = "請登入!";
                 return RedirectToAction("Login", "Account");
-            }
+            }*/
 
-            var searchDpm = await _context.DOCTOR_H
-                            .Where(result => result.Hospital_id == hospital)
-                            .GroupBy(result => result.Doctor_specialization)
-                            .Select(group => group.Key)
-                            .ToListAsync();
-            ViewBag.Hospital = hospital;
+            var searchDpm = _context.DOCTOR_H
+                .GroupBy(result => result.Doctor_specialization)
+                .Select(group => group.Key)
+                .ToList();
             ViewData["Department"] = searchDpm;
-            return View();
-        }
-        public async Task<IActionResult> reservTable(string department, string hospital)
-        {
-            var reservations = await _context.RESERVATION_H
-            .Join(
-                _context.DOCTOR_H,
-                reserv => reserv.Doctor_id,   // 使用病歷表中的 Doctor_id
-                doctor => doctor.Doctor_id,                  // 使用醫師表中的 Doctor_id
-                (reserv, doctor) => new
-                {
-                    reserv.Reserv_id,
-                    reserv.Patient_id,
-                    doctor.Doctor_id,
-                    reserv.Reserv_time,
-                    reserv.Reserv_stat,
-                    doctor.Doctor_specialization,
-                    doctor.Hospital_id
-                }
-            )
-            .Where(x => x.Doctor_specialization == department && x.Hospital_id == hospital)
-            .OrderBy(x => x.Reserv_time)
-            .ToListAsync();
-            
-            ViewData["Department"] = department;
-            ViewData["Reservations"] = reservations;
-            return View();
+            return View(searchDpm);
         }
     }
 }
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b03bff403ab66cb9efd05a83cfd6e09c79de5a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ebbd043335f2ab0e39cd0222ebcc7a1aa5440c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
